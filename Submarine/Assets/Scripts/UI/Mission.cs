5ef2f19b92cd24a360ec3bc1b8a801e8a6fcc7d2
hojoon018 <skewed018@gmail.com>
2025-03-14 22:51:57 +0900

    [Feat] 미션 UI 구현
    미션 UI X버튼 누르면 사라지거나 10초 뒤에 사라지게끔 구현
    간단하게 이미지랑 버튼 구현, 나중에 교체예정

---
+++ Submarine/Assets/Scripts/UI/Mission.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mission : MonoBehaviour
+{
+    public GameObject missionImage;
+    public Button deleteButton;
+
+    private float deleteTime = 0f;
+
+    void Start()
+    {
+        missionImage.SetActive(true);
+        deleteButton.onClick.AddListener(HideMission);
+    }
+
+    void Update()
+    {
+        deleteTime += Time.deltaTime;
+
+        if (deleteTime > 10f)
+        {
+            missionImage.SetActive(false);
+        }
+    }
+
+    void HideMission()
+    {
+        missionImage.SetActive(false);
+    }
+
+}
