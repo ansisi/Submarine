7efa455056d3a4394f3996336b13cfe5d936ad36
hojoon018 <skewed018@gmail.com>
2025-03-26 02:45:16 +0900

    [Feat] 미션UI 동적으로 생성 구현

--- Submarine/Assets/Scripts/System/GameManager.cs
+++ Submarine/Assets/Scripts/System/GameManager.cs
@@ -45,6 +45,7 @@ public class GameManager : MonoBehaviour
             UIManager.Instance.SetupResourceUI(missionParts, requiredParts);
         }
 
+        Mission.SetupMissionUI(missionParts, requiredParts);
     }
 
     private void Update()
