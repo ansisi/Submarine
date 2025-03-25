d9a38dd1dc44867086037fc2913237bd8cfc5b6b
hojoon018 <skewed018@gmail.com>
2025-03-25 21:08:42 +0900

    [Feat] UIManager 동적으로 생성 구현
    파이프, 전선도 구현했지만 UI이미지 없어서 수정 예정

---
+++ Submarine/Assets/Scripts/UI/ResourceManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance { get; private set; }
+
+    public List<Sprite> partIcons; // 부품별 아이콘 리스트 (Inspector에서 설정)
+    private Dictionary<PartType, Sprite> iconDictionary = new Dictionary<PartType, Sprite>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Enum 값과 아이콘 매핑
+        PartType[] allParts = (PartType[])System.Enum.GetValues(typeof(PartType));
+        for (int i = 0; i < allParts.Length; i++)
+        {
+            if (i < partIcons.Count)
+            {
+                iconDictionary[allParts[i]] = partIcons[i];
+            }
+        }
+    }
+
+    public Sprite GetPartIcon(PartType partType)
+    {
+        return iconDictionary.ContainsKey(partType) ? iconDictionary[partType] : null;
+    }
+}
