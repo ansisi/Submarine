522a9cb9b5fe057a28ff99a997343a2982d3caf9
tkddnr0524 <tkddnr0110@naver.com>
2025-05-13 11:22:40 +0900

    [Fix] Wave 데이터 스크립터블 오브젝트 스크립트 미적용 버그 수정
    WaveDataSO 클래스가 WaveManager 스크립트 안에 통합으로 다 작성되어 있어서 스크립터블 오브젝트에 적용되어야할 WaveDataSO 스크립트가 생기질 않음
    따로 분리하여 적용되도록 수정

--- Submarine/Assets/Scripts/System/Defense/WaveManager.cs
+++ Submarine/Assets/Scripts/System/Defense/WaveManager.cs
@@ -89,31 +89,5 @@ public class WaveManager : MonoBehaviour
     public bool IsWaveRunning() => isWaveRunning;
 }
 
-[CreateAssetMenu(fileName = "WaveData", menuName = "Wave/Wave Data")]
-public class WaveDataSO : ScriptableObject
-{
-    [Header("웨이브 지속 시간")]
-    public float waveDuration = 180f;
 
-    [Header("서브 웨이브 리스트")]
-    public List<SubWaveData> subWaves;
-}
-
-[System.Serializable]
-public class SubWaveData
-{
-    [Header("웨이브 시작 후 스폰될 시간 (초)")]
-    public float spawnTime; // 예: 30f이면 웨이브 시작 후 30초에 스폰
-
-    [Header("적 스폰 데이터(적, 스폰 개수)")]
-    public EnemySpawnData[] enemySpawnDatas; // 이 소웨이브에서 스폰할 적 종류와 수량
-    
-}
-
-[System.Serializable]
-public class EnemySpawnData
-{
-    public GameObject enemyPrefab; // 스폰할 적 프리팹
-    public int spawnCount;          // 몇 마리 스폰할지
-}
 
